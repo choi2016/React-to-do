db3da40c4889504da07d647da9a259a04dd32d97
Edward Choi <edwardchoi.2011@gmail.com>
2018-02-21 15:36:47 -0800

    changed onchange to onClick

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -6,7 +6,7 @@
        <li>
          <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
          <span>{ this.props.description }</span>
-         <button onChange={ this.props.deleteToDo }>Delete</button>
+         <button onClick={ this.props.deleteToDo }>Delete</button>
        </li>
      );
    } 
